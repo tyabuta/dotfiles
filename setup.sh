a15b1fb987f92c3262ed3f9daa4280c5024c18be
tyabuta <gmforip@gmail.com>
2012-11-16 00:11:42 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+PWD=`pwd`
+COMMAND="$1"
+case $COMMAND in
+    clean)
+        rm "$HOME.profile"
+        rm "$HOME.tmux_conf"
+        rm "$HOME.vimrc"
+        rm "$HOME.vim"
+        ;;
+    *)
+        ln -snf "$PWD/profile" "$HOME/.profile"
+        ln -snf "$PWD/tmux_conf" "$HOME/.tmux_conf"
+        ln -snf "$PWD/vimrc" "$HOME/.vimrc"
+        ln -snf "$PWD/vim" "$HOME/.vim"
+esac
+
