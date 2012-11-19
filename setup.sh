8cc41e05f96a455d6613e1df422c6130146f69d9
tyabuta <gmforip@gmail.com>
2012-11-20 06:57:25 +0900

    udpate setup.sh

--- setup.sh
+++ setup.sh
@@ -4,14 +4,14 @@ PWD=`pwd`
 COMMAND="$1"
 case $COMMAND in
     clean)
-        rm "$HOME.profile"
-        rm "$HOME.tmux_conf"
-        rm "$HOME.vimrc"
-        rm "$HOME.vim"
+        rm "$HOME/.profile"
+        rm "$HOME/.tmux_conf"
+        rm "$HOME/.vimrc"
+        rm "$HOME/.vim"
         ;;
     *)
         ln -snf "$PWD/profile" "$HOME/.profile"
-        ln -snf "$PWD/tmux_conf" "$HOME/.tmux_conf"
+        ln -snf "$PWD/tmux.conf" "$HOME/.tmux.conf"
         ln -snf "$PWD/vimrc" "$HOME/.vimrc"
         ln -snf "$PWD/vim" "$HOME/.vim"
 esac
