1fb2f48fd1c5a9e6fe50ca59f22257aef5cda7bc
tyabuta <gmfor@gmail.com>
2013-04-16 16:22:28 +0900

    update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
+
 # リンクファイルを削除
-rm "$HOME/.profile"
-rm "$HOME/.tmux.conf"
-rm "$HOME/.vimrc"
-rm "$HOME/.vim"
+rm "$HOME/.profile"   > /dev/null 2>&1
+rm "$HOME/.tmux.conf" > /dev/null 2>&1
+rm "$HOME/.vimrc"     > /dev/null 2>&1
+rm "$HOME/.vim"       > /dev/null 2>&1
 
 # バックアップに保管していたファイルを配置
-[ -d ./backup ] && cp ./backup/* $HOME/
+[ -d ./backup ] && cp ./backup/.* $HOME/ > /dev/null 2>&1
 
 exit 0
 
