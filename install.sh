df539337ea2b230f1166da111d17b5623cdabf38
tyabuta <gmforip@gmail.com>
2013-06-09 23:55:38 +0900

    OSX用の設定ファイル配置に対応

--- install.sh
+++ install.sh
@@ -24,6 +24,16 @@ function link(){
     [ -e "$src" ] && ln -snf "$src"   "$dst" && echo "link $dst"
 }
 
+# -------------------------------------------------------------------
+# dotfilesディレクトリ内にあるファイルからリンクを張る。
+# $1: リンク元のファイル名
+# $2: リンク先のファイル名
+# -------------------------------------------------------------------
+function link2(){
+    src="$PWD/$1"
+    dst="$HOME/$2"
+    [ -e "$src" ] && ln -snf "$src"   "$dst" && echo "link $dst"
+}
 
 # ファイルのバックアップ
 echo "Begin to backup files."
@@ -36,10 +46,16 @@ backup .emacs.d
 # リンクを張る
 echo "Beginning to make link files."
 link profile
-link tmux.conf
 link vimrc
 link vim
 link emacs.d
+
+if [ "Darwin" = "$(uname)" ]; then
+    link2 tmux-osx.conf .tmux.conf
+else
+    link tmux.conf
+fi
+
 
 exit 0
 
